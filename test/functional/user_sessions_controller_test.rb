e127d6b2eb16f0db5837981d452cb2e853db23dd
Brian Wisti <brian.wisti@gmail.com>
2013-02-07 20:32:36 -0800

    test that new user sessions are created

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -5,4 +5,12 @@ class UserSessionsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  test "should create new session" do
+    u = User.create!(login: 'user', 
+                     password: 'password', 
+                     password_confirmation: 'password')
+    put :create, user_session: { login: 'user', password: 'password' }
+    assert_redirected_to root_path
+  end
 end
