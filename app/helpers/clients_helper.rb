2c4cbdb6860d14a73d76ae6719156dfd3128ab9a
Brian Wisti <brian.wisti@gmail.com>
2013-03-20 05:54:19 -0700

    adjust client summary view

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,11 @@
 module ClientsHelper
+  def small_avatar_of(client)
+    default_url = "https://s3.amazonaws.com/elasticbeanstalk-us-east-1-820256515611/marys-place/avatars/avatar-blank-small.png"
+    if can? :show, client
+      link_to image_tag(default_url), client
+    else
+      image_tag(default_url)
+    end
+  end
+
 end
