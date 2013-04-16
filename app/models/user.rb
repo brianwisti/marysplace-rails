5d14091ee057cf0cbc1d000e6cae23048337055c
Brian Wisti <brian.wisti@gmail.com>
2013-04-15 21:03:19 -0700

    basic paperclip user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :login, :name, :email, :password, :password_confirmation
+  attr_accessible :avatar
+
   acts_as_authentic do |c|
     c.login_field = 'login'
     c.validate_email_field = false
@@ -13,6 +15,14 @@ class User < ActiveRecord::Base
   # Some users are also clients
   has_one :client,
     foreign_key: :login_id
+
+  has_attached_file :avatar, 
+    styles: {
+      thumb:  '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+    },
+    default_url: "https://s3.amazonaws.com/elasticbeanstalk-us-east-1-820256515611/marys-place/avatars/:style/blank.png"
 
   def role?(role)
     self.roles.exists?( name: role.to_s )
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130313120828) do
+ActiveRecord::Schema.define(:version => 20130416034221) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -136,6 +136,10 @@ ActiveRecord::Schema.define(:version => 20130313120828) do
     t.string   "last_login_ip"
     t.datetime "created_at",                          :null => false
     t.datetime "updated_at",                          :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
