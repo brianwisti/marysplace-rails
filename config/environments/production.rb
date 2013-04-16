536fe1ec35e1444a5ec2a92140b8fb065d889f0c
Brian Wisti <brian.wisti@gmail.com>
2013-04-15 21:26:18 -0700

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Marysplace::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Manage S3 uploads via paperclip
-  config.paperclip.defaults = {
+  config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
       bucket:            ENV['AWS_BUCKET'],
