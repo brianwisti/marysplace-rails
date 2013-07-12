234f6dd32563a27bfbb86630f526cf55bd526060
Brian Wisti <brian.wisti@gmail.com>
2013-07-11 21:38:11 -0700

    switch to db sessions; only show email notifications once per session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Marysplace::Application.config.session_store :cookie_store, key: '_marysplace_session'
+#Marysplace::Application.config.session_store :cookie_store, key: '_marysplace_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Marysplace::Application.config.session_store :active_record_store
+Marysplace::Application.config.session_store :active_record_store,
+  key: 'mp_session',
+  expire_after: 6.hours
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130628023047) do
+ActiveRecord::Schema.define(:version => 20130712043242) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -113,6 +113,16 @@ ActiveRecord::Schema.define(:version => 20130628023047) do
     t.integer "role_id"
     t.integer "user_id"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "signup_entries", :force => true do |t|
     t.integer  "signup_list_id"
