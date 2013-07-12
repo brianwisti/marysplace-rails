f18cc27e3171aea2093651851968cc917d047181
Brian Wisti <brian.wisti@gmail.com>
2013-07-11 22:23:18 -0700

    clear session on logout; change wording on email missing notification.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -19,6 +19,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     current_user_session.destroy
+    reset_session
     flash[:notice] = "Logout successful!"
     redirect_back_or_default new_user_session_url
   end
