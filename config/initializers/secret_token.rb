b900a3e185d8a561c3fb8abd976eb2e322652787
Brian Wisti <brian.wisti@gmail.com>
2013-07-20 15:00:03 -0700

    switch to env for rails token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Marysplace::Application.config.secret_token = '6d0c3a3e89566ad5b32fe3601b821f7823ebf9a4522258b8f127a4a5df6f9149f37fc93f66ffcfc5d569049189e912e53a8e57e5f1440913c724412d4321006d'
+Marysplace::Application.config.secret_token = ENV['SECRET_TOKEN']
