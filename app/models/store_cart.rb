d3e64674abd242a2b11924f3e78b0e89536dbecb
Brian Wisti <brian.wisti@gmail.com>
2013-07-21 17:41:30 -0700

    StoreCart.start(shopper, handler) for starting a new cart.

--- app/models/store_cart.rb
+++ app/models/store_cart.rb
@@ -1,3 +1,10 @@
 class StoreCart < ActiveRecord::Base
   attr_accessible :finished_at, :handled_by_id, :shopper_id, :started_at, :total
+
+  def self.start(shopper, handler)
+    StoreCart.create(shopper_id:    shopper.id,
+                     handled_by_id: handler.id,
+                     started_at:    DateTime.now,
+                     total:         0)
+  end
 end
--- test/unit/store_cart_test.rb
+++ test/unit/store_cart_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class StoreCartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:staff)
+    @client = clients(:amy_a)
+  end
+
+  test "start" do
+    assert StoreCart.respond_to?(:start),
+      "StoreCart.start is the blessed way to start shopping"
+    assert cart = StoreCart.start(@client, @user),
+      "StoreCart.start is called with the shopper Client and the handler User"
+    assert cart.started_at,
+      "StoreCart.start sets started_at for the new cart."
+    assert_equal 0, cart.total,
+      "StoreCart.start sets total to zero for the new cart."
+  end
 end
