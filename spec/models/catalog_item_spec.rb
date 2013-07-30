2608cd62e0ec794acc20bedf1af307351c073085
Brian Wisti <brian.wisti@gmail.com>
2013-07-30 10:02:52 -0700

    CatalogItem name spec

--- spec/models/catalog_item_spec.rb
+++ spec/models/catalog_item_spec.rb
@@ -2,6 +2,30 @@ require 'spec_helper'
 
 describe CatalogItem do
 
+  before(:all) do
+    @user = User.create do |u|
+      u.login                 = "admin"
+      u.password              = "waffle"
+      u.password_confirmation = "waffle"
+    end
+  end
+
+  context "the name" do
+    it "must be present" do
+      expect(CatalogItem.new).to have(1).errors_on(:name)
+    end
+
+    it "must be unique" do
+      item = CatalogItem.create do |i|
+        i.name     = "General"
+        i.cost     = 1000
+        i.added_by = @user
+      end
+
+      expect(CatalogItem.new(name: "General")).to have(1).errors_on(:name)
+    end
+  end
+
   context "the cost" do
     it "must be present" do
       expect(CatalogItem.new).to have(2).errors_on(:cost)
@@ -22,9 +46,9 @@ describe CatalogItem do
     item = CatalogItem.create do |i|
       i.name     = "General"
       i.cost     = 1000
-      i.added_by = user
+      i.added_by = @user
     end
 
-    expect(item.added_by).to eq(user)
+    expect(item.added_by).to eq(@user)
   end
 end
