845d0e1a3910b4342bf3ff8f650f2f3f3d06e4c1
Brian Wisti <brian.wisti@gmail.com>
2013-09-07 23:23:32 -0700

    simplistic location address information

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130817224202) do
+ActiveRecord::Schema.define(:version => 20130908054852) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -98,9 +98,14 @@ ActiveRecord::Schema.define(:version => 20130817224202) do
   add_index "clients", ["login_id"], :name => "index_clients_on_login_id"
 
   create_table "locations", :force => true do |t|
-    t.string   "name",       :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "name",         :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "phone_number"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "postal_code"
   end
 
   create_table "messages", :force => true do |t|
--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -2,6 +2,11 @@
 
 FactoryGirl.define do
   factory :location do
-    sequence(:name) { |n| "location_#{n}" }
+    sequence(:name)         { |n| "location_#{n}" }
+    sequence(:phone_number) { |n| sprintf "206-555-%04d", n }
+    sequence(:address)      { |n| sprintf "#{n} Main ST" }
+    city "Seattle"
+    state "WA"
+    postal_code "98101"
   end
 end
