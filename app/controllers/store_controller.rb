ba9df37e74c9178404689b1ccd295cffc40fe183
Brian Wisti <brian.wisti@gmail.com>
2013-11-08 07:38:12 -0800

    clean up adding cart items

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -49,33 +49,23 @@ class StoreController < ApplicationController
   def add
     # TODO: Replace with some sort of routing.
     authorize! :create, StoreCartItem
-    @errors = []
 
-    @cart = StoreCart.find(params[:id])
-    @errors.push("Unable to find cart") unless @cart
+    @cart        = StoreCart.find(params[:id])
+    catalog_item = CatalogItem.find(params[:item_id])
+    cost         = params[:item_cost].to_i
+    detail       = params[:item_detail]
 
-    item_id = params[:item_id].to_i
-    catalog_item = CatalogItem.where(id: item_id).first
-    @errors.push("Unable to find catalog item") unless catalog_item
+    cart_item = @cart.items.create do |item|
+      item.catalog_item = catalog_item
+      item.cost         = cost
 
-    cost = params[:item_cost].to_i
-    @errors.push("Cost must be greater than zero") if cost < 1
-
-
-    detail = params[:item_detail] || nil
-
-    if @errors.empty?
-      @cart.items.create do |item|
-        item.catalog_item = catalog_item
-        item.cost         = cost
-        item.detail       = detail
+      if detail
+        item.detail     = detail
       end
+    end
 
-      if @store_cart_item
-        flash[:notice] = "Item added to cart"
-      end
-    else
-      flash[:alert] = @errors.join('<br>').html_safe
+    if @store_cart_item
+      flash[:notice] = "Item added to cart"
     end
 
     redirect_to store_show_path(@cart)
