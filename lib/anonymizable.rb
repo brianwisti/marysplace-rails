53cbee4a6b73503568b030cdcb23b2308ac8779e
Brian Wisti <brian.wisti@gmail.com>
2014-04-01 14:30:04 -0700

    Anonymization methods have names now.

--- lib/anonymizable.rb
+++ lib/anonymizable.rb
@@ -1,12 +1,23 @@
 # Simplifies definition of anonymization rules for a class
 module Anonymizable
   def self.included base
-    base.extend Hooks
+    base.send :include, Messages
+    base.extend Rules
   end 
 
-  module Hooks
+  module Rules
     # Defines a rule for anonymizing a specific field in an instance
     def anonymizes
+    end
+
+    # Returns my current rules for anonymizing an instance
+    def anonymization_rules
+    end
+  end
+
+  module Messages
+    # Anonymizes my fields in accordance to my `anonymizes` rules
+    def anonymize!
     end
   end
 end
--- spec/domain/anonymizable_spec.rb
+++ spec/domain/anonymizable_spec.rb
@@ -2,17 +2,26 @@ require 'anonymizable'
 
 class AnonymizableThing
   include Anonymizable
+
+  attr_accessor :my_data
 end
 
 describe Anonymizable do
+  subject { AnonymizableThing }
 
   describe ".anonymizes" do
-    subject { AnonymizableThing }
-
     it { should respond_to(:anonymizes) }
   end
 
+  describe ".anonymization_rules" do
+    it { should respond_to(:anonymization_rules) }
+  end
+
   describe "#anonymize!" do
+    subject { AnonymizableThing.new }
+
+    it { should respond_to(:anonymize!) }
+
     context "with no anonymization hooks"
 
     context "with anonymization hooks"
