33b3bcba50d962db7463d1961e3cc95b249e7dd4
Brian Wisti <brian.wisti@gmail.com>
2014-04-05 17:35:14 -0700

    use PointsEntry.daily_count in dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,9 @@ class WelcomeController < ApplicationController
   before_filter :require_user
 
   def index
-    @client_flags = ClientFlag.order('created_at').limit(5)
+    @client_flags  = ClientFlag.order('created_at').limit(5)
     @checkin_count = Checkin.today.count
+    @entry_count   = PointsEntry.daily_count
 
     if current_user.email.blank? and !session[:email_alert_shown]
       @email_alert_needed = true
