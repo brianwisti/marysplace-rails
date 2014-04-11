51ce9aac26bb0153f730e35f441facf50662d621
Brian Wisti <brian.wisti@gmail.com>
2014-04-11 05:30:16 -0700

    adjustments for passing controller tests

--- app/models/client_note.rb
+++ app/models/client_note.rb
@@ -1,7 +1,7 @@
 class ClientNote < ActiveRecord::Base
   belongs_to :client
   belongs_to :user
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :user_id, :client_id
 
   validates :content,
     presence: true
