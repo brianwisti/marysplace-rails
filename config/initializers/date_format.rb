5ebc5296e8b62ce21cde2cc83a7f95b78e7ce77e
Brian Wisti <brian.wisti@gmail.com>
2014-04-12 09:33:09 -0700

    set default date string format

--- config/initializers/date_format.rb
+++ config/initializers/date_format.rb
@@ -3,11 +3,11 @@
 # ----------------------------
 #Date::DATE_FORMATS[:default] = "%Y-%m-%d"  # 2013-11-03
 #Date::DATE_FORMATS[:default] = "&proc"     # November 3rd, 2013
-Date::DATE_FORMATS[:default] = "%B %e, %Y" # November 3, 2013
+#Date::DATE_FORMATS[:default] = "%B %e, %Y" # November 3, 2013
 #Date::DATE_FORMATS[:default] = "%e %b %Y"  # 3 Nov 2013
 #Date::DATE_FORMATS[:default] = "%Y%m%d"    # 20131103
 #Date::DATE_FORMATS[:default] = "%e %b"     # 3 Nov
-#Date::DATE_FORMATS[:default] = ""          # custom
+Date::DATE_FORMATS[:default] = :long_ordinal          # custom
 
 # DateTime
 # ----------------------------
