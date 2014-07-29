fad82fb19f2b0ebac04efafb9ef859a8c85e24a0
Brian Wisti <brian.wisti@gmail.com>
2014-07-29 14:40:47 -0700

    picture is a preference

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,8 +1,10 @@
 class PreferencesValidator < ActiveModel::Validator
   def validate record
+    valid_fields = Client.column_names + [ "picture" ]
+
     fields = record.client_fields
     fields.each do |field|
-      unless Client.column_names.include? field
+      unless valid_fields.include? field
         record.errors[:client_fields] << "#{field} is not a valid client field"
       end
     end
