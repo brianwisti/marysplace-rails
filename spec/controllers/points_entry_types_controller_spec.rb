cad1f2aba30e90e772df30a45ac321cfac6ce02c
Brian Wisti <brian.wisti@gmail.com>
2014-09-29 05:11:06 -0700

    test /points_entry_types#show loads associated points entries

--- spec/controllers/points_entry_types_controller_spec.rb
+++ spec/controllers/points_entry_types_controller_spec.rb
@@ -4,8 +4,22 @@ describe PointsEntryTypesController do
   setup :activate_authlogic
   let(:staff_user) { create :staff_user }
 
-  describe "JSON" do
+  describe "show" do
+    let(:entry_type) { create :points_entry_type }
 
+    before do
+      create :points_entry, points_entry_type: entry_type
+      login staff_user
+    end
+
+    it "should include entries of this type" do
+      get :show, id: entry_type
+      expect(assigns(:points_entries)).to_not be_nil
+    end
+
+  end
+
+  describe "JSON" do
     context "GET /points_entry_types.json" do
       before do
         50.times { create :points_entry_type }
