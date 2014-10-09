d52ecf98ba8708073aa83f4694df7c5ef54643ac
Brian Wisti <brian.wisti@gmail.com>
2014-10-08 18:40:05 -0700

    /user_sessions - specify layout once

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,20 +1,22 @@
 class UserSessionsController < ApplicationController
+  layout 'bare'
+
   before_filter :require_no_user, only: [ :new, :create ]
   before_filter :require_user, only: :destroy
 
   def new
     @user_session = UserSession.new
-    render layout: "bare"
   end
 
   def create
     @user_session = UserSession.new(params[:user_session])
+
     if @user_session.save
       flash[:notice] = "Login successful!"
       redirect_back_or_default root_path
     else
       flash[:error] = "Login unsuccessful."
-      render :new, layout: "bare"
+      render :new
     end
   end
 
@@ -24,4 +26,5 @@ class UserSessionsController < ApplicationController
     flash[:notice] = "Logout successful!"
     redirect_back_or_default new_user_session_url
   end
+
 end
